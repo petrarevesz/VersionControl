bf2c3d518e5f6de11107da40c7152d6bfa2520d1
Petra Révész <petra.revesz99@gmail.com>
2020-10-18 14:41:34 +0200

    5 feladat

--- IRF_GYAK6/Entities/RateData.cs
+++ IRF_GYAK6/Entities/RateData.cs
@@ -8,8 +8,8 @@ namespace IRF_GYAK6.Entities
 {
     class RateData
     {
-        DateTime Date;
-        string Currency;
-        decimal Value;
+      public  DateTime Date;
+       public string Currency;
+      public   decimal Value;
     }
 }
