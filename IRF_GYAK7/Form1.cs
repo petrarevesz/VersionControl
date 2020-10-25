9f77f41d75d9822c557baf19c1df6fe57dabcaea
Petra Révész <petra.revesz99@gmail.com>
2020-10-25 17:56:08 +0100

    4 feladat

--- IRF_GYAK7/Form1.cs
+++ IRF_GYAK7/Form1.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using IRF_GYAK7.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +14,47 @@ namespace IRF_GYAK7
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
             InitializeComponent();
+
+            List<Person> Population = new List<Person>();
+            List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+            List<DeathProbability> DeathProbabilities = new List<DeathProbability>(); */
+
+
+            Population = GetPopulation(@"C:\Temp\nép.csv");
+        //    BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
+          //  DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+
+            
         }
+        public List<Person> GetPopulation(string csvpath)
+        {
+            List<Person> population = new List<Person>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    population.Add(new Person()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NbrOfChildren = int.Parse(line[2])
+                    });
+                }
+            }
+
+            return population;
+        }
+
+
+
+
+
+
     }
 }
