d8e70bb7c0a27c2413c85232893c873d6bcb5f39
Petra Révész <petra.revesz99@gmail.com>
2020-10-25 20:41:17 +0100

    7 feladat

--- IRF_GYAK7/Form1.cs
+++ IRF_GYAK7/Form1.cs
@@ -74,7 +74,40 @@ namespace IRF_GYAK7
 
 
 
+        private void SimStep(int year, Person person)
+        {
+            
+            if (!person.IsAlive) return;
 
+            
+            byte age = (byte)(year - person.BirthYear);
+
+            
+            double pDeath = (from x in DeathProbabilities
+                             where x.Gender == person.Gender && x.Age == age
+                             select x.P).FirstOrDefault();
+            
+            if (rng.NextDouble() <= pDeath)
+                person.IsAlive = false;
+
+            
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                
+                double pBirth = (from x in BirthProbabilities
+                                 where x.Age == age
+                                 select x.P).FirstOrDefault();
+                
+                if (rng.NextDouble() <= pBirth)
+                {
+                    Person újszülött = new Person();
+                    újszülött.BirthYear = year;
+                    újszülött.NbrOfChildren = 0;
+                    újszülött.Gender = (Gender)(rng.Next(1, 3));
+                    Population.Add(újszülött);
+                }
+            }
+        }
 
     }
 }
