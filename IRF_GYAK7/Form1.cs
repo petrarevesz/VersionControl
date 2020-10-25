3ae75d9599ca031228f28d746281516f076050e7
Petra Révész <petra.revesz99@gmail.com>
2020-10-25 17:59:49 +0100

    5 feladat

--- IRF_GYAK7/Form1.cs
+++ IRF_GYAK7/Form1.cs
@@ -14,21 +14,23 @@ namespace IRF_GYAK7
 {
     public partial class Form1 : Form
     {
-        
+        Random rng = new Random(1234); 
+
         public Form1()
         {
             InitializeComponent();
 
             List<Person> Population = new List<Person>();
             List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
-            List<DeathProbability> DeathProbabilities = new List<DeathProbability>(); */
+            List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
 
 
             Population = GetPopulation(@"C:\Temp\nép.csv");
-        //    BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
-          //  DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+            //    BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
+            //  DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
 
-            
+           
+
         }
         public List<Person> GetPopulation(string csvpath)
         {
