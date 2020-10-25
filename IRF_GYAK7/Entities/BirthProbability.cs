26ff9b5810e4111baa6e836bb9f515461d0d1a5d
Petra Révész <petra.revesz99@gmail.com>
2020-10-25 17:13:44 +0100

    3 feladat

---
+++ IRF_GYAK7/Entities/BirthProbability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_GYAK7.Entities
+{
+    public class BirthProbability
+    {
+    public int Age { get; set; }
+    public int NbrOfChildren { get; set; }
+
+    public double BrthProbability { get; set; }
+    }
+}
---
+++ IRF_GYAK7/Entities/DeathProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_GYAK7.Entities
+{
+    class DeathProbability
+    {
+        public int Age { get; set; }
+        public double DthProbability { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
