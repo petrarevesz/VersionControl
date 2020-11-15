e8e4f1cf6209ba5c7cd5993a1a7c2708b148f3d8
Petra Révész <petra.revesz99@gmail.com>
2020-11-15 11:16:04 +0100

    9 feladat 2.0

---
+++ IRF_GYAK8/Entities/CarFactory.cs
@@ -0,0 +1,17 @@
+﻿using IRF_GYAK8.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRF_GYAK8.Entities
+{
+    public class CarFactory : IToyFactory
+    {
+        public Toy CreateNew()
+        {
+            return new Car();
+        }
+    }
+}
